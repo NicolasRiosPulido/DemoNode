13148f6c7abfecad1f6143f152f2e98ffd0a2bae
NIcolasRiosPulidoUnoSquare <45575584+NicolasRiosPulido@users.noreply.github.com>
2023-02-07 17:35:10 -0500

    test connection to db

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 var port = process.env.PORT || 3000;
 var express = require('express');
+var mysql = require('mysql');
 
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send({
-    Output: "Hello World!",
-    desdeNode: 'aca desde node Nicolas'
+
+  var connection = mysql.createConnection({
+    host: 'database-2.cnwt5f9pbnof.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'Nicolas86'
+  });
+
+  connection.connect(function (err) {
+    let conection = ''
+    if (err) {
+      conection = 'error connecting';
+    } else {
+      conection = 'connected';
+    }
+    res.send({
+      Output: "Hello World!",
+      desdeNode: 'aca desde node Nicolas',
+      conection
+    });
   });
 });
 
